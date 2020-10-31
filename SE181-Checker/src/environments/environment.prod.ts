af1ae9187bd387ef7581ddefc602b5fe3da385f5
Tri Le <michaelle7312@gmail.com>
2020-10-31 13:47:45 -0400

    config(firebase): added firebase configurations. next is DbProvider

--- SE181-Checker/src/environments/environment.prod.ts
+++ SE181-Checker/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDjV5oA2po9Y73AJe8-3fbFry4jU-zPSq4",
+    authDomain: "attackoncheckers.firebaseapp.com",
+    databaseURL: "https://attackoncheckers.firebaseio.com",
+    projectId: "attackoncheckers",
+    storageBucket: "attackoncheckers.appspot.com",
+    messagingSenderId: "924347691842",
+    appId: "1:924347691842:web:8c38449bddf5cfb6c58edb",
+    measurementId: "G-KVT9S7ENH6"
+  }
 };
