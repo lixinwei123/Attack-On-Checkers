5adf849f978fce6367a2906ab91ebf80fc4e9cf0
Sahithi Pisupati <sasahithi@gmail.com>
2020-10-31 15:29:39 -0400

    Added background image, centered buttons, fixed button CSS, and added hover effect

--- SE181-Checker/src/app/app-routing.module.ts
+++ SE181-Checker/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'join-game',
+    loadChildren: () => import('./join-game/join-game.module').then( m => m.JoinGamePageModule)
   },
+
 ];
 
 @NgModule({
---
+++ SE181-Checker/src/app/join-game/join-game.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-join-game',
+  templateUrl: './join-game.page.html',
+  styleUrls: ['./join-game.page.scss'],
+})
+export class JoinGamePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
