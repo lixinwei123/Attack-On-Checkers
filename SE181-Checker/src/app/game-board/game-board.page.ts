543f9abc2bb0a5179823d4b14909d1c1b373ca95
lixinwei123 <lixinwei123@yahoo.com>
2020-11-14 15:39:32 -0500

    addeed kevin

--- SE181-Checker/src/app/game-board/game-board.page.ts
+++ SE181-Checker/src/app/game-board/game-board.page.ts
@@ -9,18 +9,26 @@ import { Component, OnInit } from '@angular/core';
 //capture empty cell coordinate, send the coordinate via firebase to update other player's move
 //needs to populate checkerSquares by determining whether if one is black side or white side.
 //
+// { game
+
 export class GameBoardPage implements OnInit {
-   checkerSquares = []; 
-   isPlayerWhite = true; //This variable should be set from firebase upon making game via randomization 
+   checkerSquares = []; //board
+   isPlayerWhite = true; //whoever creates the game should be the white player 
    isPieceSelected = false; 
    selectedPiece: any;
+   isWhiteMove = true;
   constructor() {
-    // this.initialBlackSide();
     console.log(this.checkerSquares);
    }
 
   ngOnInit() {
+    this.initializePlayer();
     this.initializeBoard();
+  }
+
+  //if the player is host, dont change anything, else changew isplayerwhite to false 
+  initializePlayer(){
+    
   }
   
   initializeBoard(){
@@ -40,10 +48,10 @@ export class GameBoardPage implements OnInit {
         let squareObj = {
           row: row,
           col: col,
-          isEmpty: false,
-          hasPiece:false,
-          isWhite: this.isPlayerWhite,
-          isKing: false
+          isEmpty: false, //is white square or dark square
+          hasPiece:false, //check to see if this location has any piece
+          isWhite: this.isPlayerWhite,  // purpose is to rendar board at different location depending on the player
+          isKing: false //if the piece is kinged 
         };
         if((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)){
           squareObj.isEmpty = true;
@@ -65,17 +73,24 @@ export class GameBoardPage implements OnInit {
         }
         rowList.push(squareObj);
       }
-      this.checkerSquares.push(rowList)
+      this.checkerSquares.push(rowList) //push to firebase, only if player is white.
     }
+
+    //push this.checkerSquares to firebase under a new node which can be: {gameId: {board:checkerSquares}} 
   }
 
     selectPiece(squareObj){
       this.isPieceSelected = true;
       this.selectedPiece =squareObj
+      console.log("piece selected!",squareObj)
     }
 
   //push this to firebase, make sure that checkerSquares list is subscribed to the changes of firebase
   makeMove(squareObj){
+    if(!this.isPieceSelected || squareObj.hasPiece == true || this.selectedPiece.isWhite != this.isWhiteMove){
+      return;
+    }
+
     let row, col,row2,col2;
     //make sure that the perspecive position is correct 
     if(this.isPlayerWhite){
@@ -89,12 +104,24 @@ export class GameBoardPage implements OnInit {
       row = squareObj.row;
       col = squareObj.col;
     }
-
-    if(this.isPieceSelected && squareObj.hasPiece == false && !squareObj.isEmpty && ((row2 - 1 == row ) || (this.selectedPiece.isKing && (row2 -1 == row ||row2 + 1 == row)))){
-      this.checkerSquares[row][col].hasPiece = true
-      this.checkerSquares[row][col].isWhite = this.selectedPiece.isWhite;
-      this.checkerSquares[row2][col2].hasPiece = false
+    if(this.isWhiteMove){
+      if(!squareObj.isEmpty && ((row2 - 1 == row ) || (this.selectedPiece.isKing && (row2 -1 == row ||row2 + 1 == row)))){
+        this.checkerSquares[row][col].hasPiece = true
+        this.checkerSquares[row][col].isWhite = this.selectedPiece.isWhite;
+        this.checkerSquares[row2][col2].hasPiece = false
+        this.isWhiteMove = !this.isWhiteMove;
+        console.log("isPlayerWhiteMove",this.isWhiteMove)
+      }
+    }else{
+      if(!squareObj.isEmpty && ((row2 + 1 == row ) || (this.selectedPiece.isKing && (row2 + 1 == row ||row2 - 1 == row)))){
+        this.checkerSquares[row][col].hasPiece = true
+        this.checkerSquares[row][col].isWhite = this.selectedPiece.isWhite;
+        this.checkerSquares[row2][col2].hasPiece = false
+        this.isWhiteMove = !this.isWhiteMove;
+        console.log("isPlayerWhiteMove",this.isWhiteMove)
+      }
     }
+
     if(row == 0){
       this.checkerSquares[row][col].isKing = true
       console.log("kinged!")
