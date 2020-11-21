0c1d1b2d38ea644f249b12c6da11951a4ccb9d33
Tri Le <michaelle7312@gmail.com>
2020-11-21 15:43:48 -0500

    username is now in join page like in Among Us

--- SE181-Checker/src/app/join-game/join-game.page.ts
+++ SE181-Checker/src/app/join-game/join-game.page.ts
@@ -1,21 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { DbService } from '../services/db.service';
+import { ToastController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { map, mergeMap, switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-join-game',
   templateUrl: './join-game.page.html',
   styleUrls: ['./join-game.page.scss'],
 })
 export class JoinGamePage implements OnInit {
+  username: string;
+  username$: Observable<string>;
 
   constructor(private route: Router,
     private authService: AuthService,
+    private dbService: DbService,
+    private toastCtrl: ToastController,
     ) { }
 
   ngOnInit() {
+    this.username$ = this.authService.getUserId().pipe(
+      switchMap(uid => this.dbService.getObjectValues<string>(`usernames/${uid}`))
+    )
   }
 
-  updateUsername(username: string) {
+  updateUsername() {
+    this.authService.getUserId().subscribe(async (uid) => {
+      if (this.username) {
+        this.dbService.updateObjectAtPath(`usernames`, {[uid]: this.username})
+        const toast = await this.toastCtrl.create({
+          message: `Username updated!`,
+          duration: 1000
+        })
+        toast.present();
+
+      }
+    })
 
   }
  
